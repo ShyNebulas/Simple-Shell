92ccbce828c5eaf35354bc166e1c5a0f349b73ac
Cameron Bell <cameron.bell.2020@uni.strath.ac.uk>
2022-01-25 13:56:21 +0000

    Added main.c with comment structure and modified README to include details on issues and stages

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CS210-Group-3-Shell
 
+## Issues
+
+## Stages
+- [ ] Stage 1
+- [ ] Stage 2
+- [ ] Stage 3
+- [ ] Stage 4
+- [ ] Stage 5
+- [ ] Stage 6
+- [ ] Stage 7
+- [ ] Stage 8
+- [ ] Stage 9
---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    // Find the user home directory from the environment
+    // Set current working directory to user home directory
+    // Save the current path
+
+    // Load history
+    // Load aliases
+
+    // Do while shell has not terminated
+        // Display prompt
+        // Read and parse user input
+        // While the command is a history invocation or alias then replace it with the appropriate command from history or the aliased command respectively
+        // If command is built-in invoke appropriate function
+        // Else execute command as an external process
+    // End while
+
+    // Save history
+    // Save aliases
+
+    // Restore original path
+
+    // Exit
+   
+    printf("Hello, World!");
+
+    return 0;
+
+}
